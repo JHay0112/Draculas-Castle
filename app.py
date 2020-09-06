c7600626f8421c09f490418fc0193b4ac0b73e4c
Jordan Hay <jordanhay0112@gmail.com>
2020-09-06 18:00:22 +1200

    Setup app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+'''
+
+    app.py
+
+    Author: Jordan Hay
+    Date: 2020-09-06
+
+    Dracula's Castle application/game
+
+'''
+
+# - Imports
+
+# -- Libraries
+# (Modules others have made)
+import tkinter as tk # GUI
+from tkinter import ttk # Refined GUI elements
+
+# -- Components
+# (Modules I have made)
+
+# - Classes
+
+# - Functions
+
+# - Main
+
+root = tk.Tk() # Tkinter root object
+
+# Main check
+if __name__ == "__main__":
+
+    # Tkinter setup
+    root.geometry("1000x700+100+100")
+    root.title("Dracula's Castle")
+
+    # Tkinter mainloop
+    root.mainloop()
