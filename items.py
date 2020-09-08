73064da4ab7fd446086ac0997643cc40115c81db
Jordan Hay <jordanhay0112@gmail.com>
2020-09-08 12:10:13 +1200

    Revert "Basic implementation of item types as children"

--- items.py
+++ items.py
@@ -16,32 +16,7 @@
 # The base Item class
 class Item:
 
-    # - __init__()
-    # Initialise Item Object
-    #
-    # self
-    # name (str) - The name of the item
-    def __init__(self, name):
-
-        # Set item attributes
-        self._name = name
-
-# Weapon class, child of Item
-class Weapon(Item):
-
-    # - __init__()
-    # Initialise Weapon Object
-    #
-    # self
-    # name (str) - The name of the weapon
-    # attack_damage (int) - The damage the weapon does
-    def __init__(self, name, attack_damage):
-
-        # Set weapon attributes
-        self._attack_damage = attack_damage
-
-        # Set attributes associated with parent item
-        super().__init__(name)
+    pass
 
 # Inventory class, used for item management
 class Inventory:
