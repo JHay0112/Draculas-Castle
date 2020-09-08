28a9591bdd6acee3f65aa5ff11ad5712f7caa254
Jordan Hay <jordanhay0112@gmail.com>
2020-09-08 12:36:36 +1200

    Basic setup of items

--- items.py
+++ items.py
@@ -34,11 +34,45 @@ class Weapon(Item):
     #
     # self
     # name (str) - The name of the weapon
-    # attack_damage (int) - The damage the weapon does
-    def __init__(self, name, attack_damage):
+    # damage (list [min, max]) - The min and max damage
+    def __init__(self, name, damage):
 
         # Set weapon attributes
-        self._attack_damage = attack_damage
+        self._damage = damage
+
+        # Set attributes associated with parent item
+        super().__init__(name)
+
+# Armour class, child of item
+class Armour(Item):
+
+    # - __init__()
+    # Initialise Armour Object
+    #
+    # self
+    # name (str) - The name of the armour
+    # protection (int) - Amount of damage the armour will protect from
+    def __init__(self, name, protection):
+
+        # Set armour attributes
+        self._protection = protection
+
+        # Set attributes associated with parent item
+        super().__init__(name)
+
+# Potion class, child of Item
+class Potion(Item):
+
+    # - __init__()
+    # Initialise Potion Object
+    #
+    # self
+    # name (str) - The name of the potion
+    # health_effect (init) - Amount of health the item will give the user
+    def __init__(self, name, health_effect):
+
+        # Set armour attributes
+        self._health_effect = health_effect
 
         # Set attributes associated with parent item
         super().__init__(name)
