aa860947b78356486d0763f63e517e9e45d1a8bf
Jordan Hay <jordanhay0112@gmail.com>
2020-09-09 22:30:12 +1200

    Basic room implementation

--- map.py
+++ map.py
@@ -21,4 +21,39 @@ class Map:
 # Rooms in a map
 class Room:
 
-    pass
+    # - __init__()
+    # Initialise a room object
+    # 
+    # self
+    # name (str) - Name of the room
+    # n (Room) - Which room the North entrance goes to, default None
+    # s (Room) - Which room the South entrance goes to, default None
+    # e (Room) - Which room the East entrance goes to, default None
+    # w (Room) - Which room the West entrance goes to, default None
+    # key (str) - The name of the key the room requires to unlock, default None
+    def __init__(self, name, n = None, s = None, e = None, w = None, key = None):
+
+        # Set Room attributes
+        self._name = name
+        self._n = n
+        self._s = s
+        self._e = e 
+        self._w = w 
+        self._key = key
+        self._gui = None
+
+    # - key()
+    # Returns the key value
+    #
+    # self
+    def key(self):
+        
+        return(self._key)
+
+    # - gui()
+    # Returns the gui object
+    #
+    # self
+    def gui(self):
+
+        pass
